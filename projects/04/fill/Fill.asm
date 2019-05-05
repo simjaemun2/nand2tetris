58a9c82b4fe4198641549934629f8731b4960c77
simjaemun2 <simjaemun2@naver.com>
2019-05-05 14:52:19 +0900

    project 4

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,41 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+    @8192 // 512 * 256 / 16
+    D = A
+    @pixel_bound
+    M = D
+(LOOP)
+    @i
+    M = 0
+(INNER)
+    @KBD
+    D = M
+    @WHITE
+    D; JEQ
+//(BLACK)
+    @i
+    D = M
+    @SCREEN
+    A = A + D // move i-th pixel of SCREEN
+    M = -1
+    @NEXT
+    0; JMP
+(WHITE)
+    @i
+    D = M
+    @SCREEN
+    A = A + D // move i-th pixel of SCREEN
+    M = 0
+    @NEXT
+    0; JMP
+(NEXT)
+    @i
+    MD = M + 1 // i = i + 1, D = i
+    @pixel_bound
+    D = D - M
+    @LOOP
+    D; JEQ // pixel_bound - i == 0 goto LOOP else(pixel_bound - i > 0) goto INNER
+    @INNER
+    0;JMP
--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,35 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+
+// R[2] = R[0] * R[1]
+// i = 1
+// while(i <= R[1])
+//   R[2] += R[0]
+//   i += 1
+
+    @2
+    M = 0
+    @i
+    M = 1
+
+(LOOP)
+    @i
+    D = M // D = i
+    @1 // M = R[1]
+    D = D - M // D = i - R[1]
+    @END
+    D; JGT // If i - R[1] > 0 goto END
+    @0 // M = R[0]
+    D = M // D = R[0]
+    @2 // M = R[2]
+    M = M + D // R[2] = R[2] + R[0]
+    @i // M = i
+    M = M + 1 // i = i + 1
+    @LOOP
+    0; JMP
+(END)
+    @END
+    0; JMP
+
